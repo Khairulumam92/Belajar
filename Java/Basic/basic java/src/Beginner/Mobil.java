1be68912f2c2edd726146a0b1bda9092e35c59ef
khairulumam92 <khairulumamku92@gmail.com>
2025-01-15 19:32:56 +0700

    update

--- Java/Basic/basic java/src/Beginner/Mobil.java
+++ Java/Basic/basic java/src/Beginner/Mobil.java
@@ -4,10 +4,11 @@ public class Mobil {
     // Atribut
     String merek;
     String model;
+    String roda;
     int tahun;
 
     // Metode
     void tampilkanInfo() {
-        System.out.println("Merek: " + merek + ", Model: " + model + ", Tahun: " + tahun);
+        System.out.println("Merek: " + merek + ", Model: " + model + ", Jumlah roda :" + roda ", Tahun: " + tahun);
     }
 }
