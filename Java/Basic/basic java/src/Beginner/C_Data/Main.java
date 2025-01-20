4a968c76d449019450678f05034d67f3d071dc56
khairulumam92 <khairulumamku92@gmail.com>
2025-01-20 20:48:05 +0700

    update

---
+++ Java/Basic/basic java/src/Beginner/C_Data/Main.java
@@ -0,0 +1,12 @@
+package Beginner.C_Data;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arrayAngka = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        // Mengambil data array
+        System.out.println("===Mengambil data array===");
+        System.out.println("Data array index ke-0: " + arrayAngka[0]); // Output: 1
+        System.out.println("Data array index ke-5: " + arrayAngka[5]); // Output: 6
+    }
+}
