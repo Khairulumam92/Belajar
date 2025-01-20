4afffe7e8bd7c055caba9012fe3219aa0a7d5aee
khairulumam92 <khairulumamku92@gmail.com>
2025-01-20 21:11:59 +0700

    Add methods to calculate sum, copy, and find max of an array

--- Java/Basic/basic java/src/Beginner/C_Data/Main.java
+++ Java/Basic/basic java/src/Beginner/C_Data/Main.java
@@ -8,5 +8,50 @@ public class Main {
         System.out.println("===Mengambil data array===");
         System.out.println("Data array index ke-0: " + arrayAngka[0]); // Output: 1
         System.out.println("Data array index ke-5: " + arrayAngka[5]); // Output: 6
+
+        // Menampilkan jumlah semua elemen array
+        System.out.println("===Jumlah semua elemen array===");
+        System.out.println("Jumlah: " + sumArray(arrayAngka)); // Output: 55
+
+        // Menampilkan array yang disalin
+        System.out.println("===Array yang disalin===");
+        int[] copiedArray = copyArray(arrayAngka);
+        for (int num : copiedArray) {
+            System.out.print(num + " ");
+        }
+        System.out.println(); // Output: 1 2 3 4 5 6 7 8 9 10
+
+        // Menampilkan nilai maksimum dalam array
+        System.out.println("===Nilai maksimum dalam array===");
+        System.out.println("Nilai maksimum: " + findMax(arrayAngka)); // Output: 10
+    }
+
+    // Metode untuk menghitung jumlah semua elemen array
+    public static int sumArray(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    // Metode untuk menyalin array
+    public static int[] copyArray(int[] array) {
+        int[] newArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        return newArray;
+    }
+
+    // Metode untuk menemukan nilai maksimum dalam array
+    public static int findMax(int[] array) {
+        int max = array[0];
+        for (int num : array) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
     }
 }
