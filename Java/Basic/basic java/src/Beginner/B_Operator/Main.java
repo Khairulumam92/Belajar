74be25afa44869828ae233126a69695b0eb09720
khairulumam92 <khairulumamku92@gmail.com>
2025-01-20 11:45:07 +0700

    update

---
+++ Java/Basic/basic java/src/Beginner/B_Operator/Main.java
@@ -0,0 +1,11 @@
+package Beginner.B_Operator;
+
+public class Main {
+    public static void main(String[] args) {
+        // Operator Aritmatika
+
+        System.out.println("===operasi penambahan===");
+        Method penjumlahan = new penjumlahan();
+        Met.penjumlahan();
+    }
+}
---
+++ Java/Basic/basic java/src/Beginner/B_Operator/Method.java
@@ -0,0 +1,26 @@
+package Beginner.B_Operator;
+
+public class Method {
+    int a = 10;
+    int b = 5;
+
+    void penjumlahan() {
+        int hasil = a + b;
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+
+    void pengurangan() {
+        int hasil = a - b;
+        System.out.println("Hasil pengurangan: " + hasil);
+    }
+
+    void perkalian() {
+        int hasil = a * b;
+        System.out.println("Hasil perkalian: " + hasil);
+    }
+
+    void pembagian() {
+        int hasil = a / b;
+        System.out.println("Hasil pembagian: " + hasil);
+    }
+}
