ce369de1b03559b159587cb75dad1e7875a51a42
khairulumam92 <khairulumamku92@gmail.com>
2025-01-26 17:32:13 +0700

    update

--- Java/Basic/basic java/src/Beginner/B_Operator/Nilai Rata-Rata/nilaiMapel.java
+++ Java/Basic/basic java/src/Beginner/B_Operator/Nilai Rata-Rata/nilaiMapel.java
@@ -18,9 +18,16 @@ public class nilaiMapel {
         System.out.print("Masukkan nilai Biologi : ");
         Biologi = scanner.nextFloat();
 
-        float rataRata = (Matematika + Fisika + Biologi) / 3;
+        float rataRata = ((Matematika * 0.4f ) + (Fisika * 0.3f) + (Biologi * 0.2f)) / 3;
+        // float rataRata = (Matematika + Fisika  + Biologi) / 3;
+        // float bobotMatematika = 0.4f;
+        // float bobotFisika = 0.3f;
+        // float bobotBiologi = 0.3f;
 
-        System.out.println("Nilai rata-rata adalah : " + rataRata);
+        // float nilaiAkhir = (Matematika * bobotMatematika) + (Fisika * bobotFisika) + (Biologi * bobotBiologi);
+
+        System.out.println("Nilai rata-rata adalah : %.2f" + rataRata);
+        // System.out.println("Nilai akhir adalah : " + nilaiAkhir);
 
     }
 }
