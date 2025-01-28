b95da725421f2a9ed074b5a0fd8ba5abd5fb004a
khairulumam92 <khairulumamku92@gmail.com>
2025-01-28 20:17:49 +0700

    update

--- Java/Basic/basic java/src/Beginner/E_Loop/Penghitung Angka/hitungAngkaN.java
+++ Java/Basic/basic java/src/Beginner/E_Loop/Penghitung Angka/hitungAngkaN.java
@@ -7,14 +7,11 @@ public class hitungAngkaN {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int angkaN = 0;
-        int totalAngka = 0, angkaAwal = 1;
+        int totalAngka = 0;
+        int angkaAwal = 1;
 
         System.out.print("Masukkan Angka N : ");
         angkaN = input.nextInt();
-
-        for (int i = angkaAwal; i <= angkaN; i++) {
-            totalAngka += i;
-        }
 
         System.out.println("=== Hasil ===");
         System.out.println("Jumlah angka dari 1 sampai " + angkaN + " adalah : " + totalAngka);
