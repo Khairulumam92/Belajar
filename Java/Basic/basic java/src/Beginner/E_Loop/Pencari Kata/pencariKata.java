0cc9ccb8e7f3156c4712d7d8c941fe24127812e2
khairulumam92 <khairulumamku92@gmail.com>
2025-01-28 20:30:53 +0700

    Add word search functionality to pencariKata

--- Java/Basic/basic java/src/Beginner/E_Loop/Pencari Kata/pencariKata.java
+++ Java/Basic/basic java/src/Beginner/E_Loop/Pencari Kata/pencariKata.java
@@ -1,7 +1,32 @@
 package Beginner.E_Loop;
 
+import java.util.Scanner;
+
 public class pencariKata {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("=== Pencari Kata ===");
+        System.err.print("Masukkan Kalimat (max 5): ");
+        String kalimat = input.nextLine();
+        System.out.print("Masukkan Kata yang dicari : ");
+        String kata = input.nextLine();
+
+
+
+
+        System.out.println("=== Hasil ===");
+        System.out.println("Kata " + kata + " ditemukan sebanyak " + countWord(kalimat, kata) + " kali");
+
+
+    }
+
+    public static int countWord(String kalimat, String kata) {
+        int count = 0;
+        int index = 0;
+        while ((index = kalimat.indexOf(kata, index)) != -1) {
+            count++;
+            index += kata.length();
+        }
+        return count;
     }
 }
