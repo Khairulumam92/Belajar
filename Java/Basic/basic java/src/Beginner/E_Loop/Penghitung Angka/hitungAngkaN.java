fcf9efeba6633d3c93a7fee81c5340464538478d
khairulumam92 <khairulumamku92@gmail.com>
2025-02-06 22:00:16 +0700

    update

--- Java/Basic/basic java/src/Beginner/E_Loop/Penghitung Angka/hitungAngkaN.java
+++ Java/Basic/basic java/src/Beginner/E_Loop/Penghitung Angka/hitungAngkaN.java
@@ -14,5 +14,6 @@ public class hitungAngkaN {
 
         System.out.println("=== Hasil ===");
         System.out.println("Jumlah angka dari 1 sampai " + angkaN + " adalah : " + totalAngka);
+        System.out.println("terimakasih");
     }
 }
