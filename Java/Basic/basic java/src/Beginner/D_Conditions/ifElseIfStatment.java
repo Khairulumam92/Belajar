32a30a7b2ccd3efba811ba671d2b6a4f6cd7f69e
khairulumam92 <khairulumamku92@gmail.com>
2025-02-07 05:26:19 +0700

    add initial implementation of if-else statement examples

---
+++ Java/Basic/basic java/src/Beginner/D_Conditions/ifElseIfStatment.java
@@ -0,0 +1,6 @@
+package Beginner.D_Conditions.ifElseIfStatment;
+
+public class ifElseIfStatment {
+    public static void main(String[] args) {
+    }
+}
---
+++ Java/Basic/basic java/src/Beginner/D_Conditions/ifElseStatment.java
@@ -0,0 +1,15 @@
+package Beginner.D_Conditions;
+
+public class ifElseStatment {
+    public static void main(String[] args) {
+        int nilaiPertama = 20, nilaiKedua = 10;
+
+        if (nilaiPertama > nilaiKedua) {
+            System.out.println("Nilai " + nilaiPertama + " lebih besar dari nilai " + nilaiKedua);
+        } else {
+            System.out.println("Nilai pertama lebih kecil dari nilai kedua");
+        }
+        System.out.println("Selesai");
+
+    }
+}
