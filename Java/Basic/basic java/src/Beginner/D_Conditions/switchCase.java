cb8cb17908c2614e2706fd5c4ae3782d4b3796f4
khairulumam92 <khairulumamku92@gmail.com>
2025-02-09 05:42:41 +0700

    add switchCase.java to implement a multiple-choice quiz program using switch-case statements

---
+++ Java/Basic/basic java/src/Beginner/D_Conditions/switchCase.java
@@ -0,0 +1,34 @@
+package Beginner.D_Conditions;
+
+import java.util.Scanner;
+
+public class switchCase {
+    public static void main(String[] args) {
+        System.out.println("ini adalah program switch case");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Siapakah presiden Indonesia ke-3?");
+        System.out.println("a. Bj Habibie\nb. Gus Dur\nc. Megawati\nd. Soeharto");
+        System.out.print("Soal Pilihan Ganda :");
+        String jawaban = input.next();
+
+        switch (jawaban) {
+            case "a":
+                System.out.println("Jawaban anda benar");
+                break;
+            case "b":
+                System.out.println("Jawaban anda salah");
+                break;
+            case "c":
+                System.out.println("Jawaban anda salah");
+                break;
+            case "d":
+                System.out.println("Jawaban anda salah");
+                break;
+            default:
+                System.out.println("Jawaban tidak ada");
+        }
+        input.close();
+
+    }
+}
