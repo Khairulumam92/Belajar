fcb4a97090e3f4a0c3f088fe2ec5ecf4f650269b
khairulumam92 <khairulumamku92@gmail.com>
2025-02-09 05:19:42 +0700

    add cekNilai.java to implement a grading program based on user input

---
+++ Java/Basic/basic java/src/Beginner/D_Conditions/cekNilai.java
@@ -0,0 +1,27 @@
+package Beginner.D_Conditions;
+
+import java.util.Scanner;
+
+public class cekNilai {
+    public static void main(String[] args) {
+        System.out.println("=== Program penghiitung nilai ===");
+        int nilai = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai anda: ");
+        nilai = input.nextInt();
+
+        if (nilai >= 90) {
+            System.out.println("Nilai anda A");
+        } else if (nilai >= 80) {
+            System.out.println("Nilai anda B");
+        } else if (nilai >= 70) {
+            System.out.println("Nilai anda C");
+        } else if (nilai >= 60) {
+            System.out.println("Nilai anda D");
+        } else {
+            System.out.println("Nilai anda E");
+        }
+
+    }
+}
