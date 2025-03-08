73f187abcc7362ce12aab47884a45d057ac2fb88
khairulumam92 <khairulumamku92@gmail.com>
2025-03-08 19:06:38 +0700

    remove unused binary files and add CalculatorIPK class for IPK calculation

---
+++ Java/Basic/basic java/src/Beginner/B_Operator/CalculatorIPK.java
@@ -0,0 +1,9 @@
+package Beginner.B_Operator;
+
+public class CalculatorIPK {
+
+    public static void main(String[] args) {
+        System.out.println("Kalulator Hitung IPK");
+        System.out.println("Terima kasih telah menggunakan aplikasi ini");
+    }
+}
