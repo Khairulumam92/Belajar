6c728a00c3f65346fbc8f24e10a34f06b2ece0e5
khairulumam92 <khairulumamku92@gmail.com>
2025-03-10 11:14:16 +0700

    remove separator line from CalculatorIPK output for cleaner display

--- Java/Basic/basic java/src/Beginner/B_Operator/CalculatorIPK.java
+++ Java/Basic/basic java/src/Beginner/B_Operator/CalculatorIPK.java
@@ -5,6 +5,5 @@ public class CalculatorIPK {
     public static void main(String[] args) {
         System.out.println("Kalulator Hitung IPK");
         System.out.println("Terima kasih telah menggunakan aplikasi ini");
-        System.out.println("============================================");
     }
 }
