d3dd25ef0bc281ed08760f3a0ca82be409643e2d
khairulumam92 <khairulumamku92@gmail.com>
2025-03-29 21:10:47 +0700

    add menu prompt in main method for improved user navigation

--- Java/Basic/basic java/src/Beginner/H_Encapsulation/Main.java
+++ Java/Basic/basic java/src/Beginner/H_Encapsulation/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("===Encapsulation===");
         System.out.println("Terima Kasih");
+        System.out.println("=== Menu ===");
     }
 }
